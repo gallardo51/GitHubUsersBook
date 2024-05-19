8ee34b1392b4dba4601bc3678d0aeb2c7256be88
Alex Sobolev <31108@mail.ru>
2024-05-19 16:52:11 +0300

    Initial Commit

---
+++ GitHubUsersBook/GitHubUsersBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUsersBookApp.swift
+//  GitHubUsersBook
+//
+//  Created by Александр Соболев on 19.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUsersBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
